0807343cd89ad662c76ab67f48da97994a501dc7
daniknewgarden <daniknewgarden@gmail.com>
2019-06-27 16:19:46 +0300

    Aded slider, but it has so many bugs

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,23 +1,23 @@
 $(document).ready(function(){
-
+	
 	$('.next').click(function(){
-		var currentSlide = $('.slide.curry');
-		var currentSlideIndex = $('.slider.curry').index();
-		var nextSlideIndex = currentSlideIndex + 1;
-		var nextSlide = $('.slider').eq(nextSlideIndex);
-		currentSlide.fadeOut(1000);
-		currentSlide.removeClass('curry');
+		var currentImage = $('.slider.curry');
+		var currentImageIndex = $('.slider.curry').index();
+		var nextImageindex = currentImageIndex + 1;
+		var nextImage = $('.slider').eq(nextImageindex);
+		currentImage.fadeOut(1000);
+		currentImage.removeClass('.curry');
 
-		if(nextSlideIndex == ($('.slide:last').index()+1)){
-		 $('.slider').eq(0).fadeIn(1000);
-		 $('.slider').eq(0).addClass('curry');
+
+		if(nextImageindex == ($('.slider:last').index()+1)){
+			$('.slider').eq(0).fadeIn(1000);
+			$('.slider').eq(0).addClass('curry');
+		} else {
+			nextImage.fadeIn(1000);
+			nextImage.addClass('curry');
 		}
-		else{
-		nextSlide.fadeIn(1000);
-		nextSlideIndex.addClass('curry');
-		}
+
 
 	})
+
 });
-
-
