f01f3f2ca6a9829d4c9b604a90d073a91caba520
daniknewgarden <daniknewgarden@gmail.com>
2019-08-14 18:27:08 +0300

    Aded beta version menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,25 @@
-
 $(document).ready(function(){
     $(".menu").on("click","a", function (event) {
         event.preventDefault();
         var id  = $(this).attr('href'),
             top = $(id).offset().top;
         $('body,html').animate({scrollTop: top}, 750);
+    });
+    var swiper = new Swiper('.swiper-container', {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
     });
 });
 	
