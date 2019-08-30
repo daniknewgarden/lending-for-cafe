701b6fff30f524a91db61c23ac7026f7e5bf6047
daniknewgarden <daniknewgarden@gmail.com>
2019-08-30 11:18:47 +0300

    Aded optimization  for laptops and Ipad. Replaced png image(Safari not supported webp images )

--- scripts/script.js
+++ scripts/script.js
@@ -1,69 +1,72 @@
-$(document).ready(function(){
-    $(".menu").on("click","a", function (event) {
-        event.preventDefault();
-        var id  = $(this).attr('href'),
-            top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 700);
-    });
+$(document).ready(function () {
+  $(".menu").on("click", "a", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({
+      scrollTop: top
+    }, 700);
+  });
 });
 
 
 
 
-	//E-mail Ajax Send
+//E-mail Ajax Send
 
-	$("form").submit(function() { //Change
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "scripts/mail.php", //Change
-			data: th.serialize()
-		}).done(function() {
-			$('.priatki').addClass('hidden')
-			$('.thankss').removeClass('hidden');
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
-	});
-
-
+$("form").submit(function () { //Change
+  var th = $(this);
+  $.ajax({
+    type: "POST",
+    url: "scripts/mail.php", //Change
+    data: th.serialize()
+  }).done(function () {
+    $('.priatki').addClass('hidden')
+    $('.thankss').removeClass('hidden');
+    setTimeout(function () {
+      // Done Functions
+      th.trigger("reset");
+    }, 1000);
+  });
+  return false;
+});
 
 
 
-$(document).on('click', '.daily-menu button:not(.active)', function(){
-	$(this).addClass('active').siblings().removeClass('active')
-	.closest('div.daily-menu').find('div.menu').removeClass('active').eq($(this).index()).addClass('active');
- // Set the effect type
-    var effect = 'slide';
 
-    // Set the options for the effect type chosen
-    var options = { direction: $('.menu').val() };
 
-    // Set the duration (default: 400 milliseconds)
-    var duration = 500;
+$(document).on('click', '.daily-menu button:not(.active)', function () {
+  $(this).addClass('active').siblings().removeClass('active')
+    .closest('div.daily-menu').find('div.menu').removeClass('active').eq($(this).index()).addClass('active');
+  // Set the effect type
+  var effect = 'slide';
 
-    $('#myDiv').toggle(effect, options, duration);
+  // Set the options for the effect type chosen
+  var options = {
+    direction: $('.menu').val()
+  };
+
+  // Set the duration (default: 400 milliseconds)
+  var duration = 500;
+
+  $('#myDiv').toggle(effect, options, duration);
 })
 
 
 
-    var swiper = new Swiper('.swiper-container', {
-      spaceBetween: 30,
-      centeredSlides: true,
-      autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      },
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });	
-	
+var swiper = new Swiper('.swiper-container', {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
